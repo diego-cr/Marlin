b83c701bd7706fcee7302fd90c5967216eb3c28c
diego-cr <diegocr@gmail.com>
2018-08-01 11:47:02 +0200

    LCD_ESTIMATED_TIME
    When using an LCD  displays estimated, total and remaining time.
    Will appear for 5% printing or more.
    Only calculate the time using elapsed time and sd% done.

--- Marlin/malyanlcd.cpp
+++ Marlin/malyanlcd.cpp
@@ -259,6 +259,9 @@ void process_lcd_p_command(const char* command) {
         clear_command_queue();
         quickstop_stepper();
         print_job_timer.stop();
+	#if ENABLED(LCD_ESTIMATED_TIME)      
+	  print_job_timer_lcd_estimated.stop();
+	#endif
         thermalManager.disable_all_heaters();
         #if FAN_COUNT > 0
           for (uint8_t i = 0; i < FAN_COUNT; i++) fanSpeeds[i] = 0;
