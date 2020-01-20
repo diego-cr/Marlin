32c1e34a005c059017ee0a07fa05ecd7177c8e41
diego-cr <diegocr@gmail.com>
2020-01-20 10:05:04 +0100

    LCD welcome message

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -150,6 +150,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 #define CUSTOM_MACHINE_NAME "ANYCUBIC Kossel"
+#define WELCOME_MSG "Kossel 2.0"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
