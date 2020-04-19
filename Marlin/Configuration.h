88057525e1b0429336dcc5a338bf60070a1fcb64
diego-cr <diegocr@gmail.com>
2020-04-19 18:38:24 +0200

    Delta dimensional calibration
    Instructions
    https://www.thingiverse.com/thing:1274733

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -684,7 +684,7 @@
     // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
     #define DELTA_PRINTABLE_RADIUS 116.0  // (mm)
     // Center-to-center distance of the holes in the diagonal push rods.
-    #define DELTA_DIAGONAL_ROD 267        // (mm)
+    #define DELTA_DIAGONAL_ROD 267.102     // (mm)
       // Horizontal offset from middle of printer to smooth rod center.
     #define DELTA_SMOOTH_ROD_OFFSET 186   // (mm)
     // Horizontal offset of the universal joints on the end effector.
@@ -715,7 +715,7 @@
 
   // Delta radius and diagonal rod adjustments (mm)
   //#define DELTA_RADIUS_TRIM_TOWER { 0.0, 0.0, 0.0 }
-  //#define DELTA_DIAGONAL_ROD_TRIM_TOWER { 0.0, 0.0, 0.0 }
+  #define DELTA_DIAGONAL_ROD_TRIM_TOWER { 0.311, -0.298, -0.013 }
 
 #endif
 
